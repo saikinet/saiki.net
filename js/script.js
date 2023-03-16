32564985b67e4d9e5d6bed6a7edef9412b21179b
saikinet <128045053+saikinet@users.noreply.github.com>
2023-03-16 22:46:07 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#humberger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// ketika klik di luar sidebar
+const humberger = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!humberger.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
